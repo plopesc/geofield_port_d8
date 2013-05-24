02664caf8fae93c0be617d527bca4db50cfdecb5
Brandon Morrison <brandon@brandonmorrison.com>
2013-05-24 16:52:18 -0700

    Added default field formatter for geofield, cleared out some cruft

---
+++ lib/Drupal/geofield/Plugin/field/formatter/GeofieldDefaultFormatter.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\geofield\Plugin\field\formatter\TextDefaultFormatter.
+ */
+
+namespace Drupal\geofield\Plugin\field\formatter;
+
+use Drupal;
+use geoPHP;
+use Drupal\Component\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
+use Drupal\field\Plugin\Type\Formatter\FormatterBase;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Plugin implementation of the 'geofield_default' formatter.
+ *
+ * @Plugin(
+ *   id = "geofield_default",
+ *   module = "geofield",
+ *   label = @Translation("Raw Output"),
+ *   field_types = {
+ *     "geofield"
+ *   },
+ *   settings = {
+ *     "output_format" = "wkt"
+ *   },
+ *   edit = {
+ *     "editor" = "direct"
+ *   }
+ * )
+ */
+class GeofieldDefaultFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, array &$form_state) {
+    $element['output_format'] = array(
+      '#title' => t('Output Format'),
+      '#type' => 'select',
+      '#default_value' => $this->getSetting('output_format'),
+      '#options' => array(
+        'wkt' => t('WKT'),
+        'json' => t('GeoJSON'),
+        'gpx' => t('GPX'),
+      ),
+      '#required' => TRUE,
+    );
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = array();
+    $summary[] = t('Geospatial output format: @format', array('@format' => $this->getSetting('output_format')));
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(EntityInterface $entity, $langcode, array $items) {
+    $elements = array();
+    Drupal::service('geophp');
+
+    foreach ($items as $delta => $item) {
+      $geom = geoPHP::load($item['value']);
+      $output = $geom->out($this->getSetting('output_format'));
+      $elements[$delta] = array('#markup' => $output);
+    }
+
+    return $elements;
+  }
+
+}
