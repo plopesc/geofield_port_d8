aa3d9d25c3e424100291d4ab11d219be966c752e
vdsh <vdsh@1270784.no-reply.drupal.org>
2015-11-23 19:25:20 +0100

    Issue #1957760 by RdeBoer, vdsh, Jean-Philippe Bousselin, alarcombe: Geofield D8 port

--- src/Plugin/Field/FieldFormatter/GeofieldDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/GeofieldDefaultFormatter.php
@@ -39,7 +39,7 @@ class GeofieldDefaultFormatter extends FormatterBase {
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $elements = parent::settingsForm($form, $form_state);
 
-    \Drupal::service('geophp.geophp')->getAdapterMap();
+    $options = \Drupal::service('geophp.geophp')->getAdapterMap();
     unset($options['google_geocode']);
 
     $elements['output_format'] = array(
@@ -56,7 +56,7 @@ class GeofieldDefaultFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public function settingsSummary() {
-    \Drupal::service('geophp.geophp')->getAdapterMap();
+    $formatOptions = \Drupal::service('geophp.geophp')->getAdapterMap();
     $summary = array();
     $summary[] = t('Geospatial output format: @format', array('@format' => $formatOptions[$this->getSetting('output_format')]));
     return $summary;
